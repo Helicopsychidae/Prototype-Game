0369cc8f4a88f18f1e08aad2816cf23302b33963
shuo-feng <shuofeng747@gmail.com>
2014-06-30 19:33:37 -0400

    Update main

--- src/Main.cpp
+++ src/Main.cpp
@@ -103,14 +103,31 @@ int main( int argc, char* args[] )
 		}
 		else
 		{
-			//Apply the image
-			SDL_BlitSurface( gStartUp, NULL, gScreenSurface, NULL );
+            //Main loop flag
+			bool quit = false;
 
-			//Update the surface
-			SDL_UpdateWindowSurface( gWindow );
+			//Event handler
+			SDL_Event e;
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+				}
+
+				//Apply the image
+				SDL_BlitSurface( gStartUp, NULL, gScreenSurface, NULL );
+
+				//Update the surface
+				SDL_UpdateWindowSurface( gWindow );
+			}
 		}
 	}
 
