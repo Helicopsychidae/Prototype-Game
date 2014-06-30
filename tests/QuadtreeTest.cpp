cbb75bd21885b00d360a38fa1ce5190e0a9509e3
Helicopsychidae <buchbinder.julienmarc@gmail.com>
2014-06-29 21:24:21 -0400

    Add Test

---
+++ tests/QuadtreeTest.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE QuadTree test
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <SDL.h>
+#include <Quadtree.h>
+
+BOOST_AUTO_TEST_CASE(testConstruction)
+{
+
+}
