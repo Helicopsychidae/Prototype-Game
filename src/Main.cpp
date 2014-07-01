7adace8b104365f5a30e6b0ff98f98a3af92d2bc
shuo-feng <shuofeng747@gmail.com>
2014-06-30 20:40:45 -0400

    Fix ignore .layout, clean up style in Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,45 +27,36 @@ SDL_Surface* gScreenSurface = NULL;
 //The image we will load and show on the screen
 SDL_Surface* gStartUp = NULL;
 
-bool init()
-{
+bool init() {
 	//Initialization flag
 	bool success = true;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 		success = false;
 	}
-	else
-	{
+	else {
 		//Create window
 		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
+		if( gWindow == NULL ) {
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 			success = false;
-		}
-		else
-		{
+		} else {
 			//Get window surface
 			gScreenSurface = SDL_GetWindowSurface( gWindow );
 		}
 	}
-
 	return success;
 }
 
-bool loadMedia()
-{
+bool loadMedia() {
 	//Loading success flag
 	bool success = true;
 
 	//Load splash image
 	gStartUp = SDL_LoadBMP( "startup.bmp" );
-	if( gStartUp == NULL )
-	{
+	if( gStartUp == NULL ) {
 		printf( "Unable to load image %s! SDL Error: %s\n", "startup.bmp", SDL_GetError() );
 		success = false;
 	}
@@ -73,8 +64,7 @@ bool loadMedia()
 	return success;
 }
 
-void close()
-{
+void close() {
 	//Deallocate surface
 	SDL_FreeSurface( gStartUp );
 	gStartUp = NULL;
@@ -87,22 +77,16 @@ void close()
 	SDL_Quit();
 }
 
-int main( int argc, char* args[] )
-{
+int main( int argc, char* args[] ) {
 	//Start up SDL and create window
-	if( !init() )
-	{
+	if( !init() ) {
 		printf( "Failed to initialize!\n" );
 	}
-	else
-	{
+	else {
 		//Load media
-		if( !loadMedia() )
-		{
+		if ( !loadMedia() ) {
 			printf( "Failed to load media!\n" );
-		}
-		else
-		{
+		} else {
             //Main loop flag
 			bool quit = false;
 
@@ -110,14 +94,11 @@ int main( int argc, char* args[] )
 			SDL_Event e;
 
 			//While application is running
-			while( !quit )
-			{
+			while( !quit ) {
 				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
+				while( SDL_PollEvent( &e ) != 0 ) {
 					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
+					if( e.type == SDL_QUIT ) {
 						quit = true;
 					}
 				}
